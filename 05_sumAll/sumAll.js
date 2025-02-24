cf717d534b71bc0351cfa6fe30980c8cc9cdd750
Alexi Michael <189779678+heyitsalexi@users.noreply.github.com>
2025-02-23 20:08:20 -0500

    Add: Attempts for JS exercises
    Completed the first round of the exercise on my own. Performed minor
    adjustements of answers after reviewing the solutions.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,22 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    
+    if (Number.isInteger(num1) && 
+        Number.isInteger(num2) &&
+        num1 > 0 &&
+        num2 > 0) {
+    } else {
+        return "ERROR";
+    }
 
-};
+    let sum = Math.min(num1, num2);
+
+    for (let i = Math.min(num1, num2); i < Math.max(num1, num2); i++){
+        sum += (i+1);
+    };
+
+    return sum;
+
+}
 
 // Do not edit below this line
 module.exports = sumAll;
--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,17 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(tempF) {
+
+  // Fahrenheit to Celsius: (x - 32) x 5/9
+  let tempC = ((+tempF - 32) * 5/9);
+  return Math.round(tempC * 10) / 10;
+
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempC) {
+
+  // Celsius to Fahrenheit: (x * 9/5) + 32
+  let tempF = ((tempC * 9/5) + 32);
+  return Math.round(tempF * 10) / 10;
+
 };
 
 // Do not edit below this line
