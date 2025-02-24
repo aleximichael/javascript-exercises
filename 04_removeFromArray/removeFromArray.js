6c352e7cbab4b8895932b1fe58dcd26e34f67026
Alexi Michael <189779678+heyitsalexi@users.noreply.github.com>
2025-02-23 20:09:45 -0500

    Add: Initial attempts for javascript exercises

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,17 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...elements) {
+    
+    let newArray = [];
+
+    for (let i = 0; i < array.length; i++){
+        elemIndex = elements.indexOf(array[i]);
+        // indexOf returns -1 if the value is not found
+        if (elemIndex === -1) {
+            newArray.push(array[i]);
+        };
+    };
+
+    return newArray;
+
 };
 
 // Do not edit below this line
